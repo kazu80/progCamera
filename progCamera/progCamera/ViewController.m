9fb4ee12ae96aee1f89fbcd07d504454cb6c8cc5
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-06-19 11:38:14 +0900

    Add Camera Extention
    カメラ機能追加
    ※PhotoAlbumから読み込むように変更している

--- progCamera/progCamera/ViewController.m
+++ progCamera/progCamera/ViewController.m
@@ -26,4 +26,31 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)clickCamera:(id)sender {
+    
+    UIImagePickerController *ip = [[UIImagePickerController alloc] init];
+//    ip.sourceType = UIImagePickerControllerSourceTypeCamera;
+//    ip.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    ip.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    ip.allowsEditing = YES;
+    ip.delegate = self;
+    [self presentViewController:ip animated:YES completion:nil];
+    
+}
+
+- (IBAction)clickGray:(id)sender {
+}
+
+- (IBAction)clickSepia:(id)sender {
+}
+
+- (IBAction)clickSave:(id)sender {
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+    self.imageView.image = image;
+}
+
 @end

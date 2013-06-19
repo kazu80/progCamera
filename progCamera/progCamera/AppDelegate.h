a6d51da46727c504a88c7c465b3e2714f4357c86
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-06-19 11:02:40 +0900

    make project
    プロジェクトの作成

---
+++ progCamera/progCamera/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  progCamera
+//
+//  Created by kazuyoshi kawakami on 13/06/19.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ progCamera/progCamera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  progCamera
+//
+//  Created by kazuyoshi kawakami on 13/06/19.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
